758a7f255c1b8d49c01dd5daaf040cb4a82b9bff
Gianni Floriddia <gianniparflor@gmail.com>
2025-03-25 15:22:02 +0000

    comentários melhorados

--- src/agente/Controlo.java
+++ src/agente/Controlo.java
@@ -5,6 +5,9 @@ package agente;
  * classes Agente e ControloPersonagem
  * e que contém o metodo processar.
  * Responsavel pelo papel de controlador.
+ * Implementa o processamento interno
+ * do agentee que relaciona a percecao com a acao.
+ * Implementa a "inteligencia do agente"
  */
 public interface Controlo {
     // Metodo publico processar, que recebe uma percepção e devolve uma ação.
